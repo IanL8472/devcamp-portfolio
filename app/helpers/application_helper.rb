0f9ffb58f9aa2b38709b1be5f7bb473f87053cf2
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 19:08:41 +0000

    Changed time for alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,7 +72,7 @@ module ApplicationHelper
   end
   
   def alert_generator msg
-    js add_gritter(msg, title: "Ian Lee Portfolio", sticky: false)
+    js add_gritter(msg, title: "Ian Lee Portfolio", sticky: false, time: 3000)
   end
   
 end
